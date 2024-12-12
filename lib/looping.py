c451b3c1166b7eec49c128b0d4457e57737bfd03
bscit-05-39008695 <aronmutai99@gmail.com>
2024-12-12 08:37:21 +0300

    pythonloops

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,38 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    while True:
+        print("10")
+        print("9")
+        print("8")
+        print("7")
+        print("6")
+        print("5")
+        print("4")
+        print("3")
+        print("2")
+        print("1")
+        print("Happy New Year!")
+        break
+    
+    
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    while True:
+        return [num**2 for num in int_list]
+        
 
 def fizzbuzz():
     # code goes here!
-    pass
+    while True:
+        for i in range(1,101):
+            if i % 15 == 0:
+                print("FizzBuzz")
+            elif i % 3 == 0:
+                print("Fizz")
+            elif i % 5 == 0:
+                print("Buzz")
+            else:
+                print(i)
+        break
